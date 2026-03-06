044ff79e515349a57a94526e4086b7286bf279b0
Ryan Russell <russellsprouts2@gmail.com>
2026-03-06 12:49:42 -0500

    Use lifelib to write lifehistory patterns

--- life_history.py
+++ life_history.py
@@ -1,7 +1,11 @@
 import numpy
 import re
+import lifelib
 
 from lifetree import lt
+
+session = lifelib.load_rules('sixstate.rule')
+lht = session.lifetree()
 
 EMPTY = lt.pattern()
 
@@ -63,3 +67,23 @@ def write_life_history(
     result = re.sub("(.)\\1+", lambda x: f"{len(x.group(0))}{x.group(1)}", result)
     return result
 
+def write_life_history(
+    green=EMPTY,  # on
+    blue=EMPTY,  # was on
+    white=EMPTY,  # marked, on
+    red=EMPTY,  # marked, off
+    yellow=EMPTY,  # temp mark
+    grey=EMPTY,  # boundary, always dead
+):
+    patterns = [
+        lht.pattern(grey.rle_string().replace('A', 'F'))(*(grey.getrect() or (0, 0))[0:2]),
+        lht.pattern(yellow.rle_string().replace('A', 'E'))(*(yellow.getrect() or (0, 0))[0:2]),
+        lht.pattern(red.rle_string().replace('A', 'D'))(*(red.getrect() or (0, 0))[0:2]),
+        lht.pattern(white.rle_string().replace('A', 'C'))(*(white.getrect() or (0, 0))[0:2]),
+        lht.pattern(blue.rle_string().replace('A', 'B'))(*(blue.getrect() or (0, 0))[0:2]),
+        lht.pattern(green.rle_string())(*green.getrect()[0:2]),
+    ]
+
+    p = sum(patterns, start=lht.pattern())
+
+    return p.rle_string().replace('x3xsixstate', 'LifeHistory')
