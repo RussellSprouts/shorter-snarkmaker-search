fce9bc88a93f6b8d2f2e080d61b386ec974941be
Ryan Russell <russellsprouts2@gmail.com>
2026-05-17 19:08:30 -0400

    Agnosticization support

--- compile_p120_fast.py
+++ compile_p120_fast.py
@@ -128,65 +128,87 @@ class Partial:
             case 'population':
                 return (len(self.emits), self.time) < (len(other.emits), other.time)
 
+
+def get_possible_gliders():
+    """Finds the list of simple agnosticizations for each recipe step.
+    It's a static list -- alternate gliders that are guaranteed to work
+    no matter what we've sent so far"""
+    possible_gliders = []
+    so_far = []
+    for i in range(0, len(recipe)):
+        possibilities = dag.get_next(tuple(so_far))
+        filtered_possibilities = list(filter(
+            lambda a: a.kind != 'rephase',
+            possibilities
+        ))
+        # append one of the possibilities since this is a static version
+        so_far.append(filtered_possibilities[0])
+        possible_gliders.append(filtered_possibilities)
+    return possible_gliders
+
+
 beam = [Partial(0, -float('inf'), [], [])]
-for gli in recipe:
-    gli = adjust_recipe(gli)
-    color = gli[0] % 2
-    phase = gli[1] % 2
-    gli_lane = gli[0]
+
+recipe_steps = get_possible_gliders()
+for step in recipe_steps:
     newbeam = []
-    for rec in library[(color, phase)]:
-        best = None
-        for pos in beam:
-            time = pos.time
-            orig_time = time
-            recipe_parity, *recipe_rest = rec.recipe
+    for possible_glider in step:
+        gli = adjust_recipe((possible_glider.lane, possible_glider.parity))
+        color = gli[0] % 2
+        phase = gli[1] % 2
+        gli_lane = gli[0]
+        for rec in library[(color, phase)]:
+            best = None
+            for pos in beam:
+                time = pos.time
+                orig_time = time
+                recipe_parity, *recipe_rest = rec.recipe
 
-            delay = pos.nextgl + (gli_lane - rec.offset) * 4 + recipe_parity
-            parity_offset = (delay - recipe_parity) % 8
-            emits = list(pos.emits)
-            emits_str = list(pos.emits_str)
-            nextgl = pos.nextgl
-            while delay < time + (recipe_parity - ((time + parity_offset) % 8)) % 8:
-                if delay + PERIOD >= time + (0 - ((time + parity_offset) % 8)) % 8 + 90:
-                    nextgl += PERIOD
-                    delay += PERIOD
-                    # send a 0, (90) glider
-                    time += (0 - ((time + parity_offset) % 8)) % 8
-                    emits.append(time)
-                    emits_str.extend(('0', '(90)'))
-                    time += 90
-                elif delay + PERIOD >= time + (4 - (time % 4)) % 8 + 90:
-                    nextgl += PERIOD
-                    delay += PERIOD
-                    # send a 4, (90) glider
-                    time += (4 - ((time + parity_offset) % 8)) % 8
-                    emits.append(time)
-                    emits_str.extend(('4', '(90)'))
-                    time += 90
-                else:
-                    nextgl += PERIOD * 2
-                    delay += PERIOD * 2
-                    # send a 2, (90) glider
-                    time += (2 - ((time + parity_offset) % 8)) % 8
-                    emits.append(time)
-                    emits_str.extend(('2', '(90)'))
-                    time += 90
+                delay = pos.nextgl + (gli_lane - rec.offset) * 4 + recipe_parity
+                parity_offset = (delay - recipe_parity) % 8
+                emits = list(pos.emits)
+                emits_str = list(pos.emits_str)
+                nextgl = pos.nextgl
+                while delay < time + (recipe_parity - ((time + parity_offset) % 8)) % 8:
+                    if delay + PERIOD >= time + (0 - ((time + parity_offset) % 8)) % 8 + 90:
+                        nextgl += PERIOD
+                        delay += PERIOD
+                        # send a 0, (90) glider
+                        time += (0 - ((time + parity_offset) % 8)) % 8
+                        emits.append(time)
+                        emits_str.extend(('0', '(90)'))
+                        time += 90
+                    elif delay + PERIOD >= time + (4 - (time % 4)) % 8 + 90:
+                        nextgl += PERIOD
+                        delay += PERIOD
+                        # send a 4, (90) glider
+                        time += (4 - ((time + parity_offset) % 8)) % 8
+                        emits.append(time)
+                        emits_str.extend(('4', '(90)'))
+                        time += 90
+                    else:
+                        nextgl += PERIOD * 2
+                        delay += PERIOD * 2
+                        # send a 2, (90) glider
+                        time += (2 - ((time + parity_offset) % 8)) % 8
+                        emits.append(time)
+                        emits_str.extend(('2', '(90)'))
+                        time += 90
 
-            time += (recipe_parity - ((time + parity_offset) % 8)) % 8
-            wait = ((delay - time) // 8) * 8
-            if wait != 0 and math.isfinite(wait):
-                emits_str.append(f'wait {wait}')
-            emits.append(delay)
-            for i in itertools.accumulate(recipe_rest):
-                emits.append(delay + i)
-            emits_str.extend(tuple(map(str, rec.recipe)) + (f'({rec.min_follow})',))
-            nextgl += PERIOD * rec.consumed
-            time = emits[-1] + rec.min_follow
-            new = Partial(nextgl, time, emits, emits_str)
-            heapq.heappush(newbeam, new)
-            if len(newbeam) > args.beam_width:
-                newbeam.pop()
+                time += (recipe_parity - ((time + parity_offset) % 8)) % 8
+                wait = ((delay - time) // 8) * 8
+                if wait != 0 and math.isfinite(wait):
+                    emits_str.append(f'wait {wait}')
+                emits.append(delay)
+                for i in itertools.accumulate(recipe_rest):
+                    emits.append(delay + i)
+                emits_str.extend(tuple(map(str, rec.recipe)) + (f'({rec.min_follow})',))
+                nextgl += PERIOD * rec.consumed
+                time = emits[-1] + rec.min_follow
+                new = Partial(nextgl, time, emits, emits_str)
+                heapq.heappush(newbeam, new)
+                if len(newbeam) > args.beam_width:
+                    newbeam.pop()
 
     beam = newbeam
 
